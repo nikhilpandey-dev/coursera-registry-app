d936a72144a175e914318177985ac06d85846095
Nikhil Pandey <nikhilpandey4@gmail.com>
2023-06-12 04:45:25 +0000

    Added functionaality of CRUD to the app.

--- src/routes/Registry.js
+++ src/routes/Registry.js
@@ -8,6 +8,7 @@ function Registry() {
     const [error, setError] = useState(false);
     const addItems = (e) => {
         e.preventDefault();
+        if(error) return;
 
         const tempData = [...registryData];
         tempData.push(textInput);
@@ -22,6 +23,21 @@ function Registry() {
         else setError(false);
     }, [textInput])
 
+    const removeItem = (index) => {
+        if(error) return;
+        let newData = [...registryData];
+        newData.splice(index, 1);
+        setRegistryData(newData);
+    }
+
+    const editItem = (index) => {
+        if(error) return;
+        let newData = [...registryData];
+        newData[index] = textInput;
+        setRegistryData(newData);
+
+    }
+
     console.log(registryData);
     return (
         <div>
@@ -34,6 +50,16 @@ function Registry() {
             <input type='submit' value='Submit' />
             </form>
            {error ? <span style={{color: "red"}}>Error occurred.</span> : null}
+           {
+                registryData.map((item, index) => {
+                    return (
+                        <li key={index}>{item} 
+                        <button onClick={() => removeItem(index)}>Remove</button>
+                        <button onClick={() => editItem(index)}>Update</button>
+                        </li>
+                    )
+                })
+           }
         </div>
     )
 }
