225d89a9c5abed805527273b899ce12a09dfd6d5
Nikhil Pandey <nikhilpandey4@gmail.com>
2023-06-12 03:46:26 +0000

    Completed routing in React, its different than whats being given in the video.

---
+++ src/routes/Registry.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+function Registry() {
+    return (
+        <div>
+            <h1>Registry</h1>
+            <Link to="/">Click here for Home</Link>
+        </div>
+    )
+}
+
+export default Registry;
