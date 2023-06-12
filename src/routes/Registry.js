8c9aba8877afa038b994232eeabb16b2732832aa
Nikhil Pandey <nikhilpandey4@gmail.com>
2023-06-12 04:07:04 +0000

    Added registry data new.

--- src/routes/Registry.js
+++ src/routes/Registry.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { useState } from 'react';
 import {Link} from "react-router-dom";
 function Registry() {
+    const [registryData, setRegistryData] = useState([])
+    const [textInput, setTextInput] = useState("")
+    const addItems = (e) => {
+        e.preventDefault();
+
+        const tempData = [...registryData];
+        tempData.push(textInput);
+        setRegistryData(tempData);
+        setTextInput("");
+        // console.log(registryData);
+
+    }
+
+    console.log(registryData);
     return (
         <div>
             <h1>Registry</h1>
             <Link to="/">Click here for Home</Link>
+            <form>
+                <label>Text input
+            <input type='text' value={textInput} onChange={(e) => setTextInput(e.target.value)}/>
+            </label>
+            <input type='submit' value='Submit' />
+            </form>
+           
         </div>
     )
 }
